dcd1747bb56dfff559771875dee4d86d3ac32d8e
nirara0905 <94527361+nirara0905@users.noreply.github.com>
2022-01-04 09:16:12 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,9 +16,76 @@
   //     title[i].classList.add('show')
   //   }
   // });
-  for(let i = 0;i < title.length;i++){
-    title[i].classList.add('show');
+  // window.onload = function() {
+  //   title.forEach(title => {
+  //     setTimeout(() => {
+  //       title.classList.add('show')
+  //     }, 1000);
+  //   });
+  // }
+  
+  window.onload = function() {
+    for(let i = 0;i < title.length;i++){
+      setTimeout(() => {
+        title[i].classList.add('show')
+      }, (i+1) * 100);
+    }
   }
+
+  // window.onload = function() {
+  //   setTimeout(() => {
+  //     title[0].classList.add('show')
+  //   }, 100);
+  //   setTimeout(() => {
+  //     title[1].classList.add('show')
+  //   }, 200);
+  //   setTimeout(() => {
+  //     title[2].classList.add('show')
+  //   }, 300);
+  //   setTimeout(() => {
+  //     title[3].classList.add('show')
+  //   }, 400);
+  //   setTimeout(() => {
+  //     title[4].classList.add('show')
+  //   }, 500);
+  //   setTimeout(() => {
+  //     title[5].classList.add('show')
+  //   }, 600);
+  //   setTimeout(() => {
+  //     title[6].classList.add('show')
+  //   }, 700);
+  //   setTimeout(() => {
+  //     title[7].classList.add('show')
+  //   }, 800);
+  //   setTimeout(() => {
+  //     title[8].classList.add('show')
+  //   }, 900);
+  //   setTimeout(() => {
+  //     title[9].classList.add('show')
+  //   }, 1000);
+  //   setTimeout(() => {
+  //     title[10].classList.add('show')
+  //   }, 1100);
+  //   setTimeout(() => {
+  //     title[11].classList.add('show')
+  //   }, 1200);
+  //   setTimeout(() => {
+  //     title[12].classList.add('show')
+  //   }, 1300);
+  //   setTimeout(() => {
+  //     title[13].classList.add('show')
+  //   }, 1400);
+  //   setTimeout(() => {
+  //     title[14].classList.add('show')
+  //   }, 1500);
+  //   setTimeout(() => {
+  //     title[15].classList.add('show')
+  //   }, 1600);
+  //   setTimeout(() => {
+  //     title[16].classList.add('show')
+  //   }, 1700);
+  // }
+
 
 
 }
